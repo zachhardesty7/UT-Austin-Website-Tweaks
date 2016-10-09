6d8c23a99533fb00969036c70183875f447d9e9f
zach797a <zachhardesty7@gmail.com>
2016-10-09 14:54:57 -0500

    Final commit of messing with Google documentation, rework coming

--- Chrome Extension/addClass.js
+++ Chrome Extension/addClass.js
@@ -1,8 +1,8 @@
 // currently an error due to permissions to execute. check on content scripts
 function addClass(){
   var classes = document.querySelectorAll("[data-th]");
-  var uniqueID = classes.item(0);
-  var name = classes.item(1);
+  var uniqueID = classes.item(0); //.innerHTML
+  var name = document.querySelector("#details h2")
   var time = classes.item(2);
   var para = document.createElement("p");
   var node = document.createTextNode(uniqueID + name + time);
