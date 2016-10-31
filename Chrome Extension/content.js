f2ea2cf67db94df2233a66555c65b68ca3fcc855
zach797a <zachhardesty7@gmail.com>
2016-10-31 11:48:33 -0500

    Switeched output to array, further changes to fix

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -1,55 +1,32 @@
 chrome.runtime.onMessage.addListener(
   function(message, sender, sendResponse) {
-    if (message.type == 'getClass') {
-      sendResponse(output);
+    if (message.type == 'getCourse') {
+      sendResponse(courses);
     }
     return true;
   }
 );
 
-function collectClassInfo() {
-  var classes  = document.querySelectorAll("[data-th]");
-  var name     = document.querySelector("#details h2").textContent;
-  var uniqueID = classes.item(0).textContent;
-  var day      = classes.item(1).textContent;
-  var hour     = classes.item(2).textContent;
-  output       = {
-    uniqueID: uniqueID,
-    name    : name,
-    day     : day,
-    hour    : hour
-  }
+function collectCourseInfo() {
+  var courseData  = document.querySelectorAll("[data-th]");
+  var courseName     = document.querySelector("#details h2").textContent;
+  var courseUniqueID = rawClassData.item(0).textContent;
+  var courseDay      = rawClassData.item(1).textContent;
+  var courseTime     = rawClassData.item(2).textContent;
+  if (typeof courses === 'undefined') {
+    courses = [];
+  };
+  courses.push({
+    uniqueID: courseUniqueID,
+    name    : courseName,
+    day     : courseDay,
+    time    : courseTime
+  });
+
   // is the best way really to declare this variable
   // globally? better to just call function? make function return output?
-}
+  // locally store variable
+  chrome.storage.local.set(courses);
+};
 
-collectClassInfo();
-
-
-// var elements = document.getElementsByTagName('*');
-// var count = 0;
-//
-// function tokeCounter(){
-//     for (var i = 0; i < elements.length; i++) {
-//         var element = elements[i];
-//
-//         for (var j = 0; j < element.childNodes.length; j++) {
-//             var node = element.childNodes[j];
-//
-//             if (node.nodeType === 3) {
-//                 var text = node.nodeValue;
-//                 if(text == '420'){
-//                     count++;
-//                 }
-//
-//                 var replacedText = text.replace(/420/, '+1');
-//
-//                 if (replacedText !== text) {
-//                     element.replaceChild(document.createTextNode(replacedText), node);
-//                 }
-//             }
-//         }
-//     }
-// }
-//
-// tokeCounter();
+collectCourseInfo();
--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,7 +1,7 @@
+// Receive Message, Append to Popup
 function getClassInfo() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {type: "getClass"}, function(response) {
-      
+    chrome.tabs.sendMessage(tabs[0].id, {type: "getCourse"}, function(response) {
       console.log(response);
       var para = document.createElement("p");
       para.textContent = response.name;
@@ -9,7 +9,32 @@ function getClassInfo() {
     });
   });
 }
+// need a way for the array index of recently added course
+// to be determined and messaged.
+// or skip altogether and call loadPopupClasses();
+// but it must be reworked to only append when course isn't already there
 
+
+// Listener for Add Current Class
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('add-class').addEventListener('click', getClassInfo);
 })
+
+// load info into Popup DOM
+function loadPopupClasses() {
+  chrome.storage.local.get("output", function(output) {
+      if(typeof output == "undefined") {
+        console.error("Chrome Strorage undefined var");
+      } else {
+        var para = document.createElement("p");
+        para.textContent = output.name;
+        document.getElementById("classes").appendChild(para);
+      }
+  });
+}
+
+loadPopupClasses();
+
+
+// take output/response, add new piece to global OBJ, commit obj to local storage,
+// pull obj out of local storage, commit pieces to DOM
