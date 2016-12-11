f7fa025d79bf7fd9fae3f623e6e46e5c7e56ca20
zach797a <zachhardesty7@gmail.com>
2016-12-11 11:27:58 -0600

    Added all course data to array

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -9,18 +9,27 @@ chrome.runtime.onMessage.addListener(
 );
 
 function collectCourseInfo() {
-  var rawCourseData  = document.querySelectorAll("[data-th]");
-  var courseName     = document.querySelector("#details h2").textContent;
-  var courseUniqueID = rawCourseData.item(0).textContent;
-  var courseDay      = rawCourseData.item(1).textContent;
-  var courseTime     = rawCourseData.item(2).textContent;
+  let rawCourseData    = document.querySelectorAll('[data-th]');
+  let courseName       = document.querySelector('#details h2').textContent;
+  let courseUniqueID   = rawCourseData[0].textContent;
+  let courseDay        = rawCourseData[1].textContent;
+  let courseTime       = rawCourseData[2].textContent;
+  let courseRoom       = rawCourseData[3].textContent;
+  let courseInstructor = rawCourseData[4].textContent;
+  let courseStatus     = rawCourseData[5].textContent;
+  // TODO: parse to remove wasted space / convert to more readable symbol
+  let courseFlags      = rawCourseData[6].textContent;
   courseOutput = {
-    uniqueID: courseUniqueID,
-    name : courseName,
-    day : courseDay,
-    time : courseTime
+    uniqueID  : courseUniqueID,
+    name      : courseName,
+    day       : courseDay,
+    time      : courseTime,
+    room      : courseRoom,
+    instructor: courseInstructor,
+    status    : courseStatus,
+    flags     : courseFlags
   };
-
   // is the best way really to declare this variable
   // globally? better to just call function? make function return output?
+  // and run function (add ()) after send response
 };
